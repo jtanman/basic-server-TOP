2ffb3aceb0598e78bd774c0b896e38a1c4d223e5
jtanman <jtanman@gmail.com>
2025-05-08 15:38:47 -0700

    Add views instead of static html

--- index.js
+++ index.js
@@ -4,21 +4,41 @@ const app = express();
 const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
 
 const PORT = 3000;
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
+
+// app.js
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "about", text: "About" },
+];
+
+const users = ["Rose", "Cake", "Biff"];
+
+app.get("/", (req, res) => {
+  res.render("index", { links: links, users: users });
+});
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
 app.use("/", indexRouter);
 
 // Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
 app.use((err, req, res, next) => {
-    console.error(err);
-    // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
-    res.status(err.statusCode || 500).send(err.message);
-    
-  });
+  console.error(err);
+  // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,25 +5,26 @@ const fs = require("fs");
 
 const indexRouter = Router();
 
-// Middleware to serve files dynamically
+// Middleware to serve EJS templates dynamically
 indexRouter.use((req, res, next) => {
-    const filePath = path.join(__dirname, `../${req.path === "/" ? "index" : req.path}.html`);
+    const viewName = req.path === "/" ? "index" : req.path.substring(1); // Remove leading slash
+    const filePath = path.join(__dirname, `../views/${viewName}.ejs`);
 
-    // Check if the file exists
+    // Check if the EJS file exists
     fs.access(filePath, fs.constants.F_OK, (err) => {
         if (err) {
             // If the file doesn't exist, pass to the 404 handler
             next();
         } else {
-            // If the file exists, serve it
-            res.sendFile(filePath);
+            // If the file exists, render it
+            res.render(viewName);
         }
     });
 });
 
 // Catch-all route for 404 errors
 indexRouter.use((req, res) => {
-    res.status(404).sendFile(path.join(__dirname, "../404.html"));
+    res.status(404).render("404");
 });
 
 module.exports = indexRouter;
