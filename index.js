11794bd76b3c969ba3b09dbb36965fb67ba0a6df
jtanman <jtanman@gmail.com>
2025-05-08 15:14:02 -0700

    Add error handlers

--- index.js
+++ index.js
@@ -5,11 +5,20 @@ const authorRouter = require("./routes/authorRouter");
 const bookRouter = require("./routes/bookRouter");
 const indexRouter = require("./routes/indexRouter");
 
+const PORT = 3000;
+
 app.use("/authors", authorRouter);
 app.use("/books", bookRouter);
 app.use("/", indexRouter);
 
-const PORT = 3000;
+// Every thrown error in the application or the previous middleware function calling `next` with an error as an argument will eventually go to this middleware function
+app.use((err, req, res, next) => {
+    console.error(err);
+    // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+    res.status(err.statusCode || 500).send(err.message);
+    
+  });
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
