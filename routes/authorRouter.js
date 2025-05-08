74375a39d14f6795905892f2b18493a43f39275a
jtanman <jtanman@gmail.com>
2025-05-08 11:26:02 -0700

    Add author controller middleware

--- routes/authorRouter.js
+++ routes/authorRouter.js
@@ -1,12 +1,18 @@
 // routes/authorRouter.js
+
 const { Router } = require("express");
+const { getAuthorById } = require('../controllers/authorController');
 
 const authorRouter = Router();
 
+// ... other route handlers
+authorRouter.get("/:authorId", getAuthorById);
+
+
 authorRouter.get("/", (req, res) => res.send("All authors"));
-authorRouter.get("/:authorId", (req, res) => {
-  const { authorId } = req.params;
-  res.send(`Author ID: ${authorId}`);
-});
+// authorRouter.get("/:authorId", (req, res) => {
+//   const { authorId } = req.params;
+//   res.send(`Author ID: ${authorId}`);
+// });
 
 module.exports = authorRouter;
